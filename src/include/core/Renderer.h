cc6a827f4cd6789b1dbefb549609a9c2989d60ab
Joseph Larralde <joseph.larralde@gmail.com>
2022-08-26 19:35:07 +0200

    reduce the base renderer to its strict minimum
    base renderer doesn't deal with chronologies anymore, this has to be done by child classes
    it now provides minimal functionality (i.e. only combine3 method for now) and has a pure virtual method getNextSetPair which must also be implemented in child classes
    the orphaned endings systems has been removed because Chronology should not allow orphaned endings

--- src/include/core/Renderer.h
+++ src/include/core/Renderer.h
@@ -6,206 +6,86 @@
 #include <map>
 #include "Chronology.h"
 
+// BASE RENDERER CLASS TEMPLATE ////////////////////////////////////////////////
+
 template <typename Model, typename Command, typename CommandKey>
 class Renderer {
+private:
+  std::map<CommandKey, Events::Set<Model>> pendingEndingSets;
 
-    // -------------------------------------------------------------------------
-    // --------------------------PRIVATE FIELDS---------------------------------
-    // -------------------------------------------------------------------------
+  virtual Events::SetPair<Model> getNextSetPair() = 0;
 
-    Chronology<Model> modelEvents; // A chronology of the model (partition)
+protected:
+  virtual Events::Set<Model> combine3(Command cmd) {
+    CommandKey key = Events::keyFromData<Command, CommandKey>(cmd);
+    Events::Set<Model> emptySet = { 0, {} };
 
-    Chronology<Command> commandEvents; // A chronology of the commands
+    if (Events::isStart<Command>(cmd)) {
+      auto pair = getNextSetPair();
+      Events::Set<Model> startingSet = pair.start;
+      Events::Set<Model> endingSet = pair.end;
 
-    bool lastEventPulled; // Indicates whether the last event of the model
-    // has already been pulled, so as to react differently when asked if any are left.
+      // Manage multi-controller interaction :
+      // if this same key already has events registered in the combine map,
+      // trigger them before registering the new ones.
 
-    std::list<std::vector<Model>> orphanedEndings; // A fifo of ending events
-    // that should have been associated to a key press, and have thus been thrown out.
-    // They are associated to releases which would otherwise have no effect.
-    // Note : this list should never be used under normal circumstances
-    // (because if ending events have been associated to a key press,
-    // that means the preprocessing of the model chronology went wrong.)
+      if (pendingEndingSets.find(key) != pendingEndingSets.end()) {
+        // despite it being not optimal, we use MERGE_AT_BEGINNING in order to
+        // keep all the end events first.
+        // It is OK because this is not supposed to happen frequently.
+        // todo : optimize if necessary
+        Events::mergeSets(
+          startingSet,
+          pendingEndingSets[key],
+          Events::MERGE_AT_BEGINNING
+        );
+      }
 
-    std::map<CommandKey, std::vector<Model>> map3; // A map between a start event
-    // and its correspondent ending.
+      pendingEndingSets[key] = endingSet;
+      return startingSet;
+    } else {
+      // This should never happen, except if we start rendering while
+      // some key is pressed then release this key during rendering, in which
+      // case we simply return an emptySet
+      if (pendingEndingSets.find(key) == pendingEndingSets.end()) {
+        return emptySet;
+      }
 
-    // -------------------------------------------------------------------------
+      Events::Set<Model> endingSet = pendingEndingSets[key];
+      pendingEndingSets.erase(key);
+      return endingSet;
+    }
+  }
+
+  // is this really useful (not used anywhere ATM) ?
+  // if we just return all pending endings set we miss all the
+  // "not immediately following" endings
+
+  // keeping track of all active notes to be able to generate
+  // all their corresponding endings at any moment would be more appropriate as
+  // it would allow to optimally handle an "all notes off" event.
+  // plus, this could be performed without any knowledge of pendingEndingSets,
+  // like the voice stealing stuff
+
+  // virtual Events::Set<Model> getAllPendingEndingEvents() {
+  //   Events::Set<Model> res = { 0, {} };
+
+  //   for (auto& set : pendingEndingSets) {
+  //     const auto& extraEvents = set.second.events;
+  //     res.events.insert(
+  //       res.events.end(),
+  //       extraEvents.begin(),
+  //       extraEvents.end()
+  //     );
+  //   }
+
+  //   return res;
+  // }
 
 public:
-
-    // -------------------------------------------------------------------------
-    // ----------------------CONSTRUCTORS/DESTRUCTORS---------------------------
-    // -------------------------------------------------------------------------
-
-    Renderer() : lastEventPulled(false), modelEvents(Chronology<Model>()) {}
-    Renderer(ChronologyParams::parameters params) :
-        lastEventPulled(false), modelEvents(Chronology<Model>(params)) {}
-
-    // -------------------------------------------------------------------------
-    // ---------------------------PUBLIC METHODS--------------------------------
-    // -------------------------------------------------------------------------
-
-    // Push a new model event.
-    // For now, command chronologies are not supported, so this is the only push
-
-    virtual void pushEvent(int dt, Model event) {
-        modelEvents.pushEvent(dt, event);
-    }
-
-    // This should be private if implemented at all,
-    // since commands will be pushed and pulled live
-
-    // void pushCommandEvent(int dt, Command cmd) {
-    //     commandEvents.pushEvent(dt, cmd);
-    // }
-
-    // Finalize the fixed partition.
-
-    virtual void finalize() {
-        modelEvents.finalize();
-        //std::cout << "C++ debug : " << std::endl << modelEvents << std::endl;
-    }
-
-    // If the last event isn't pulled, refer to the partition chronology.
-    // If it has, then it is still available, so there are still events.
-    // However, if the user has explicitly stated to exclude it, then it is possible.
-    // (This is necessary when checking to see if the chronology is simply empty.)
-
-    virtual bool hasEvents(bool countLastEvent = true) {
-        return modelEvents.hasEvents() || (countLastEvent&&lastEventPulled);
-    }
-
-    // Pull events from the partition chronology.
-    // Is there any use for this ??
-
-    virtual std::vector<Model> pullEvents() {
-        return modelEvents.pullEvents();
-    }
-
-    virtual Events::Set<Model> pullEventsSet() {
-        return modelEvents.pullEventsSet();
-    }
-
-    // Combine a command with the appropriate model events.
-    // The combineN methods could be invoked from live commands or by pulling
-    // the commandEvents chronology.
-
-    virtual std::vector<Model> combine3(Command cmd) {
-        CommandKey key = Events::keyFromData<Command, CommandKey>(cmd);
-        std::vector<Model> emptyEvents = {};
-
-        // If the command is a press, search for the next event.
-
-        if (Events::isStart<Command>(cmd)) {
-            //std::cout << "start command" << std::endl;
-            std::vector<Model> events = modelEvents.pullEvents();
-
-            // If these next events are not all endings :
-            // (isn't this always the case ??)
-            // (isn't it counter-intuitive to press a key and only release notes ?)
-
-            if (Events::hasStart<Model>(events)) {
-                std::vector<Model> nextEvents = emptyEvents;
-
-                // Get the associated end,
-                // which thanks to the chronology spec,
-                // is always found right next to the beginning.
-
-                try {
-                    nextEvents = modelEvents.pullEvents();
-                    if (Events::hasStart<Model>(nextEvents)) throw nextEvents;
-                } catch (std::vector<Model> nextEvents) {
-                    // nextEvents should be an ending set.
-                    std::cout << "ASSOCIATED START IN COMBINE MAP" << std::endl;
-                    exit(1);
-                }
-
-                /*std::cout << "C++ debug : " << cmd << " associated with ";
-                if(!nextEvents.empty()){
-                    for(Model& e : nextEvents){
-                        std::cout << e << std::endl;
-                    }
-                }else{
-                    std::cout << "no release events" << std::endl;
-                }*/
-
-                // Indicate that the last event has been pulled.
-                if (!modelEvents.hasEvents()) lastEventPulled = true;
-
-                // Manage multi-controller interaction :
-                // If this same key already has events registered in the combine map,
-                // Trigger them, and then register the new ones.
-
-                if (map3.find(key) != map3.end()) {
-                    std::vector<Model> extraEvents = map3[key];
-                    events.insert(events.end(), extraEvents.begin(), extraEvents.end());
-                    // Should we rather append them to nextEvents ?
-                }
-
-                // Map the key to this event, so as to bind its release to it.
-                map3[key] = nextEvents;
-                return events;
-
-            } else {
-                orphanedEndings.push_back(events);
-                if (!modelEvents.hasEvents()) lastEventPulled = true;
-                return emptyEvents;
-            }
-        } else {
-
-            //std::cout << "end command" << std::endl;
-
-            /*
-            try {
-                if (map3.find(key) == map3.end() && !lastEventPulled)
-                    throw std::runtime_error("INVALID MAP ENTRY FOR KEY");
-            } catch (std::runtime_error e) {
-                // Original idea (early 2022) :
-                // This can only happen if two controllers pressed the same key on the same channel
-                // This should not happen
-
-                // Update 08/07/22 ; this is FAR more common than we thought.
-                // For example, this happens if the system is launched with a key already held;
-                // The release of that key will trigger the exception because no note was associated with the key.
-
-                // This causes an instant segfault in the ossia binding.
-                // So the question is, should we keep it that way ?
-                std::cout << e.what() << std::endl;
-                exit(1);
-            }
-            //*/
-            
-            if (map3.find(key) == map3.end() && !lastEventPulled)
-                return emptyEvents;
-
-            std::vector<Model> events = map3[key];
-            if (events.empty() && !orphanedEndings.empty()) {
-                events = orphanedEndings.front();
-                orphanedEndings.pop_front();
-            }
-
-            map3.erase(key);
-            return events;
-        }
-    }
-
-    virtual void clear() {
-        modelEvents.clear();
-    }
-
-    // Replace the partition chronology entirely.
-    // DEEP COPY so that the original partition will be left unmodified.
-
-    void setPartition(Chronology<Model> const newPartition){
-        this->clear();
-        modelEvents = newPartition;
-    }
-
-    Chronology<Model> getPartition(){
-        return modelEvents;
-    }
-
+  virtual ~Renderer() {}
+  
+  // virtual Events::Set<Model> render(Command cmd) = 0;
 };
 
 #endif /* MFP_RENDERER_H */
